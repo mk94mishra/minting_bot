cd434ebb469b193ed78a37fa8c909cd6b5645806
mk94mishra <mk94mishra@gmail.com>
2023-10-10 05:18:26 +0530

    p1

--- proxy/common.py
+++ proxy/common.py
@@ -4,23 +4,21 @@ import sqlite3
 import datetime
 import config as config
 
-# Connect to the SQLite database
-conn = sqlite3.connect(config.database['db'])
-cursor = conn.cursor()
+
+proxy_expire_details = []
+i=0
+for bin_url in config.bin_api_list:
+    #[apiurl, weightage,weightage_minut,event_time,proxy_name]
+    proxy_expire_details.append([bin_url,0,0,0,config.proxy_name])
+    i=i+1
 
 # Get the current base URL for the proxy
 def current_base_url():
-    # Execute a SQL query to select the proxy with the specified name
-    cursor.execute(f"SELECT * FROM tbl_proxy where proxy_name='{config.database['proxy_name']}'")
-
-    # Get the results of the query
-    results = cursor.fetchall()
-
     # Iterate over the results and check if the proxy is expired or has low weightage
-    for data in results:
-        if (int(datetime.datetime.now().timestamp() * 1000) - int(data[4])) > config.custom['bin_max_time'] or int(data[2]) < config.custom['bin_max_weightage']:
+    for data in proxy_expire_details:
+        if (int(datetime.datetime.now().timestamp() * 1000) - int(data[3])) > config.custom['bin_max_time'] or int(data[1]) < config.custom['bin_max_weightage']:
             # If the proxy is expired or has low weightage, return the proxy's base URL
-            current_base_url = data[1]
+            current_base_url = data[0]
             return current_base_url
 
     # If no suitable proxy is found, return None
@@ -28,22 +26,19 @@ def current_base_url():
 
 # Update the weightage of a proxy
 def update_weightage(response, base_url):
-    # Execute a SQL query to update the weightage of the proxy
-    cursor.execute(f"UPDATE tbl_proxy set weightage = '{response['x-mbx-used-weight']}', weightage_minute = '{response['x-mbx-used-weight-1m']}', event_time='{int(datetime.datetime.now().timestamp() * 1000)}' where proxy_url='{base_url}' and proxy_name='{config.database['proxy_name']}'")
-
-    # Commit the changes to the database
-    conn.commit()
-
+    #[apiurl, weightage,weightage_minut,event_time,proxy_name]
+    [bin_url,0,0,0,config.proxy_name] 
+    filtered_list = [sub_list for sub_list in proxy_expire_details if base_url in sub_list]
+    filtered_list[0][1]=response['x-mbx-used-weight']
+    filtered_list[0][2]=response['x-mbx-used-weight-1m']
+    filtered_list[0][3]=int(datetime.datetime.now().timestamp() * 1000)
     # Return True to indicate that the update was successful
     return True
 
 # Get all proxy details
 def get_proxy_details():
-    # Execute a SQL query to select all proxies with the specified name
-    cursor.execute(f"SELECT * FROM tbl_proxy where proxy_name='{config.database['proxy_name']}'")
-
     # Get the results of the query
-    return cursor.fetchall()
+    return proxy_expire_details
 
 # Call an API using a proxy
 async def call_api(url, method, headers=None, data=None):
--- proxy/config.py
+++ proxy/config.py
@@ -7,3 +7,10 @@ custom = {
     "bin_max_time":60000, # 60 second
     "bin_max_weightage":5500  
 }
+proxy_name = 'proxy_one'
+bin_api_list = [
+    'https://api.binance.com',
+    'https://api1.binance.com',
+    'https://api2.binance.com',
+    'https://api3.binance.com'
+]
