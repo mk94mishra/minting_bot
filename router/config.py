e6e1b6e36133b5808cae9c759313d209a0bed425
mk94mishra <mk94mishra@gmail.com>
2023-10-09 17:48:35 +0530

    pr list

--- router/config.py
+++ router/config.py
@@ -13,8 +13,35 @@ custom = {
     "proxy_max_weightage":5500  
 }
 
+# proxy_url ={
+#     'proxy_one':'http://3.145.60.97:80',
+#     'proxy_two':'http://3.12.123.111:80',
+#     'proxy_three':'http://18.220.46.156:80',
+#     'proxy_four':'http://18.116.51.129:80',
+#     'proxy_five':'http://18.116.51.129:80'
+# }
+
 proxy_url ={
     'proxy_one':'http://127.0.0.1:8000',
-    'proxy_two':'http://127.0.0.1:8080',
-    'proxy_three':'http://127.0.0.1:8081'
+    'proxy_two':'http://127.0.0.1:8000',
+    'proxy_three':'http://127.0.0.1:8000',
+    'proxy_four':'http://127.0.0.1:8000',
+    'proxy_five':'http://127.0.0.1:8000'
 }
+
+
+#[apiurl, weightage,weightage_minut,event_time,proxy_name]
+proxy_details = [
+    	['https://api.binance.com',0,0,0,'proxy_one'],
+        ['https://api1.binance.com',0,0,0,'proxy_one'],
+        ['https://api2.binance.com',0,0,0,'proxy_one'],
+        ['https://api3.binance.com',0,0,0,'proxy_one'],
+        ['https://api.binance.com',0,0,0,'proxy_two'],
+        ['https://api1.binance.com',0,0,0,'proxy_two'],
+        ['https://api2.binance.com',0,0,0,'proxy_two'],
+        ['https://api3.binance.com',0,0,0,'proxy_two'],
+        ['https://api.binance.com',0,0,0,'proxy_three'],
+        ['https://api1.binance.com',0,0,0,'proxy_three'],
+        ['https://api2.binance.com',0,0,0,'proxy_three'],
+        ['https://api3.binance.com',0,0,0,'proxy_three']
+    ]
--- router/router.py
+++ router/router.py
@@ -11,7 +11,8 @@ data = {
     "url": "/api/v3/exchangeInfo",
     "method": "GET"
 }
-
+proxy_details_list = config.proxy_details
+proxy_details_dict = {}
 # Function to select a proxy from the database
 def select_proxy():
     """Selects a proxy from the database based on its expiration time and weightage.
@@ -19,19 +20,27 @@ def select_proxy():
     Returns:
         The URL of the selected proxy, or None if no suitable proxy is found.
     """
-
-    conn = sqlite3.connect(config.database['db'])
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM tbl_proxy")
-    results = cursor.fetchall()
-    cursor.close()
-    conn.close()
-
-    for data in results:
-        if (int(datetime.datetime.now().timestamp() * 1000) - int(data[4])) > config.custom['proxy_max_time'] or int(data[2]) < config.custom['proxy_max_weightage']:
-            return data[5]
+    for data in proxy_details_list:
+        if (int(datetime.datetime.now().timestamp() * 1000) - int(data[3])) > config.custom['proxy_max_time'] or int(data[2]) < config.custom['proxy_max_weightage']:
+            return data[4]
 
     return None
+
+def update_proxy(response_header): 
+
+    for response_data in response_header:
+        url = response_data[0]
+        name =  response_data[4]
+        weigtage = response_data[1]
+        weightage_one_minute = response_data[2]
+        event_time = response_data[3]
+        
+        for item in proxy_details_list:
+            if item[0] == url and item[4] == name:
+                item[1] = weigtage
+                item[2] = weightage_one_minute
+                item[3] = event_time
+    print(proxy_details_list)
 i=0
 import time
 # Function to make an API call using the specified proxy
@@ -60,6 +69,7 @@ async def make_api_call(session, data):
         with open('output.txt', 'a') as file:
             file.write(f'| {i}--{response.status}--{time.time()}\n')
             file.close()
+            header_response=response.headers
         return json_response
 
 # Function to make concurrent API calls
@@ -69,21 +79,33 @@ async def concurrent_call(start_range,end_range,data):
     Returns:
         A list of JSON responses from the API, or None if all requests failed.
     """
+    print("tutorss-",start_range,end_range)
     async with ClientSession() as session:
         tasks = [make_api_call(session, data) for i in range(start_range,end_range+1)]
         return await asyncio.gather(*tasks)
     
-total_call = 10000
+total_call = 10
 # Run the asyncio event loop and print the results
-
 async def call_all_proxy(data):
     start_range = 0
     end_range = total_call//len(config.proxy)
     results_one = await concurrent_call(start_range,end_range,data)
-    results_two = await concurrent_call(end_range+1,(end_range*2),data)
+    start_range = end_range
+    end_range = end_range+end_range
+    results_two = await concurrent_call(start_range+1,(end_range),data)
+    start_range = end_range
+    end_range = end_range+end_range
     last_call_reminder = total_call%len(config.proxy)
-    results_three = await concurrent_call((end_range*3)+1,(end_range*4)+last_call_reminder,data)
-    results = await [results_one,results_two,results_three]
+    results_three = await concurrent_call((start_range)+1,(total_call),data)
+
+    # end_range = end_range+end_range
+    # last_call_reminder = total_call%len(config.proxy)
+    # results_four = await concurrent_call((start_range)+1,(end_range),data)
+
+    # end_range = end_range+end_range
+    # last_call_reminder = total_call%len(config.proxy)
+    # results_five = await concurrent_call((start_range)+1,(total_call),data)
+    results = [results_one,results_two,results_three, results_four]
     return results
 
-result = asyncio.run(call_all_proxy())
+result = asyncio.run(call_all_proxy(data))
