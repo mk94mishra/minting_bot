002fd255b17abc16941b5cfa18d1609742b23528
mk94mishra <mk94mishra@gmail.com>
2023-10-10 05:20:13 +0530

    p2

--- proxy/config.py
+++ proxy/config.py
@@ -7,7 +7,7 @@ custom = {
     "bin_max_time":60000, # 60 second
     "bin_max_weightage":5500  
 }
-proxy_name = 'proxy_one'
+proxy_name = 'proxy_two'
 bin_api_list = [
     'https://api.binance.com',
     'https://api1.binance.com',
