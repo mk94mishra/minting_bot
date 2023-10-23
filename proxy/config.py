db4b60debc3d18918dc8ee8211d811d7f57fc861
manish <mk94mishra@gmail.com>
2023-10-23 23:17:15 +0530

    congi

--- proxy/config.py
+++ proxy/config.py
@@ -1,13 +1,9 @@
-database = {
-    "db":'../db_proxy.db',
-    "proxy_name":"proxy_one"
-}
-
 custom = {
     "bin_max_time":60000, # 60 second
     "bin_max_weightage":5500  
 }
-proxy_name = 'proxy_three'
+proxy_name = 'proxy_five'
+
 bin_api_list = [
     'https://api.binance.com',
     'https://api1.binance.com',
