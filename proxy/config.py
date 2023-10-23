0833c97ca875e4db565b0d46159bf9da4175ee90
manish <mk94mishra@gmail.com>
2023-10-23 23:33:16 +0530

    1

--- proxy/config.py
+++ proxy/config.py
@@ -2,7 +2,7 @@ custom = {
     "bin_max_time":60000, # 60 second
     "bin_max_weightage":5500  
 }
-proxy_name = 'proxy_two'
+proxy_name = 'proxy_one'
 
 bin_api_list = [
     'https://api.binance.com',
