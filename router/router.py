1f349dd0038904dd80c72886bd6758f25663e0a6
manish <mk94mishra@gmail.com>
2023-10-26 00:07:44 +0530

    add test cases

--- router/router.py
+++ router/router.py
@@ -1,48 +1,18 @@
 # Import the necessary libraries
 import asyncio
 from aiohttp import ClientSession
-import datetime
 import config
+import requests
 
+proxy_url_list = list(config.proxy_url.values())
+def select_proxy(proxy_no):
+    active_url = proxy_url_list[proxy_no]
+    return active_url
 
-# Define the data to be sent in the request
-data = {
-    "url": "/api/v3/exchangeInfo",
-    "method": "GET"
-}
-proxy_details_list = config.proxy_details
-proxy_details_dict = {}
-# Function to select a proxy from the database
-def select_proxy():
-    """Selects a proxy from the database based on its expiration time and weightage.
-
-    Returns:
-        The URL of the selected proxy, or None if no suitable proxy is found.
-    """
-    for data in proxy_details_list:
-        if (int(datetime.datetime.now().timestamp() * 1000) - int(data[3])) > config.custom['proxy_max_time'] or int(data[2]) < config.custom['proxy_max_weightage']:
-            return data[4]
-
-    return None
-
-def update_proxy(response_proxy_details): 
-
-    for response_data in response_proxy_details:
-        url = response_data[0]
-        name =  response_data[4]
-        weigtage = response_data[1]
-        weightage_one_minute = response_data[2]
-        event_time = response_data[3]
-        
-        for item in proxy_details_list:
-            if item[0] == url and item[4] == name:
-                item[1] = weigtage
-                item[2] = weightage_one_minute
-                item[3] = event_time
 i=0
 import time
 # Function to make an API call using the specified proxy
-async def make_api_call(session, data):
+async def make_api_call_concurrent(session, data, proxy_no):
     """Makes an API call using the specified proxy.
 
     Args:
@@ -53,59 +23,92 @@ async def make_api_call(session, data):
         The JSON response from the API, or None if the request failed.
     """
 
-    url = select_proxy()
-    if url is None:
-        return "proxy end"
-
-    url = config.proxy_url[url]
+    url = select_proxy(proxy_no)
+    print(url)
     url = f"{url}/call_api"
     global i  
-    
     async with session.post(url, json=data) as response:
         json_response = await response.json()
         i=i+1
-        update_proxy(json_response['proxy_details'])
         with open('output.txt', 'a') as file:
             file.write(f'| {i}--{response.status}--{time.time()}\n')
             file.close()
-        # header_response=response.headers
         return json_response
 
+
+# Function to make an API call using the specified proxy
+def make_api_call(data,proxy_no):
+    """Makes an API call using the specified proxy.
+
+    Args:
+        session: An aiohttp.ClientSession object.
+        data: A dictionary containing the request data.
+
+    Returns:
+        The JSON response from the API, or None if the request failed.
+    """
+
+    url = select_proxy(proxy_no)
+    print(url)
+    if url is None:
+        return "proxy end"
+    
+    url = f"{url}/call_api"
+        
+    response = requests.post(url, json=data)
+    with open('output.txt', 'a') as file:
+        file.write(f'| {i}--{response.status_code}--{time.time()}\n')
+        file.close()
+    json_response = response.json()
+    return json_response
+
+
 # Function to make concurrent API calls
-async def concurrent_call(start_range,end_range,data):
+async def concurrent_call(start_range,end_range,data,proxy_no):
     """Makes concurrent API calls using the specified number of requests.
 
     Returns:
         A list of JSON responses from the API, or None if all requests failed.
     """
     async with ClientSession() as session:
-        tasks = [make_api_call(session, data) for i in range(start_range,end_range+1)]
+        tasks = [make_api_call_concurrent(session, data, proxy_no) for i in range(start_range,end_range)]
         return await asyncio.gather(*tasks)
     
-total_call = 100000
-# Run the asyncio event loop and print the results
-async def call_all_proxy(data):
-    start_range = 0
-    diff_range = total_call//len(config.proxy)
-    end_range = diff_range
-    results_one = await concurrent_call(start_range,diff_range,data)
-
-    start_range = end_range
-    end_range = end_range+diff_range
-    results_two = await concurrent_call(start_range+1,(end_range),data)
     
-    start_range = end_range
-    end_range = end_range+diff_range
-    results_three = await concurrent_call((start_range)+1,(end_range),data)
+# Run the asyncio event loop and print the results
+async def call_all_proxy_concurrent(total_call,data):
+    all_results = []
+    if total_call > 20:
+        diff_call = total_call//len(config.proxy)
+        tasks = [asyncio.create_task(concurrent_call(proxy_no * diff_call,(proxy_no * diff_call) + diff_call,data,proxy_no)) for proxy_no in range(0,len(config.proxy))]
+        results = await asyncio.gather(*tasks)
+        all_results.append(results)
+    else:
+        results = await concurrent_call(0,total_call,data,0)
+        all_results.append(results)
+    return all_results
 
-    start_range = end_range
-    end_range = end_range+end_range
-    results_four = await concurrent_call((start_range)+1,(end_range),data)
+# result = asyncio.run(call_all_proxy_concurrent(total_call,data))
+# print(result)
 
-    start_range = end_range
-    results_five = await concurrent_call((start_range)+1,(total_call),data)
+def call_all_proxy_one_by_one(total_call,data):
+    if total_call > 20:
+        diff_call = total_call//len(config.proxy)
+        start_range = 0
+        end_range = diff_call
+        all_results = []
+        for proxy_no in range(0,len(config.proxy)):
+            for _ in range(start_range,end_range):
+                results = make_api_call(data,proxy_no)
+                all_results.append(results)
+            start_range = end_range
+            end_range = end_range + diff_call
+    else:
+        for _ in range(start_range,total_call):
+            results = make_api_call(data,proxy_no)
+            all_results.append(results)
+    
+    return all_results
 
-    results = [results_one,results_two,results_three,results_four,results_five]
-    return results
-
-result = asyncio.run(call_all_proxy(data))
+# results = call_all_proxy_one_by_one(total_call,data)
+# print(results)
---
+++ router/test.py
@@ -0,0 +1,69 @@
+import asyncio
+from datetime import datetime
+from router import *
+
+
+# Define the data to be sent in the request
+data = {
+    "url": "/api/v3/exchangeInfo",
+    "method": "GET"
+}
+
+
+# test 10,000 calls
+total_call = 10000
+start_time = datetime.now()
+results = call_all_proxy_one_by_one(total_call,data)
+print(f"one by one call start time{start_time} and end time{datetime.now()} in {total_call} call")
+with open('testresults.txt', 'a') as file:
+    file.write(f"one by one call start time{start_time} and end time{datetime.now()} in {total_call} call\n")
+    file.close()
+
+# test concurrent 10,000 calls
+start_time = datetime.now()
+results = asyncio.run(call_all_proxy_concurrent(total_call,data))
+print(f"concurrent call start time{start_time} and end time{datetime.now()} in {total_call} call")
+with open('testresults.txt', 'a') as file:
+    file.write(f"concurrent call start time{start_time} and end time{datetime.now()} in {total_call} call")
+    file.close()
+
+
+# test normal call in a time
+async def timeout_call_one_by_one(seconds):
+    try:
+        results = await asyncio.wait_for(call_all_proxy_one_by_one(total_call, data), timeout=seconds)  # 300 seconds = 5 minutes
+        # Process results if the call is successful within the timeout
+        if results is not None:
+            total_results = len(results)
+            print(f"Total results one by one: {total_results} in {seconds}")
+        else:
+            print("No results due to timeout.")
+    except asyncio.TimeoutError:
+        # Handle timeout (function call took more than 5 minutes)
+        print("Function call timed out after 5 minutes")
+        return None
+
+# test concurrent call in a time
+async def timeout_call_concurrent(seconds):
+    try:
+        results = await asyncio.wait_for(call_all_proxy_one_by_one(total_call, data), timeout=seconds)  # 300 seconds = 5 minutes
+        # Process results if the call is successful within the timeout
+        if results is not None:
+            total_results = len(results)
+            print(f"Total results concurrent: {total_results} in {seconds}")
+        else:
+            print("No results due to timeout.")
+    except asyncio.TimeoutError:
+        # Handle timeout (function call took more than 5 minutes)
+        print("Function call timed out after 5 minutes")
+        return None
+
+# test call in 5 minutes
+asyncio.run(timeout_call_one_by_one(300))
+asyncio.run(timeout_call_concurrent(300))
+
+# test call in 1 minutes
+asyncio.run(timeout_call_one_by_one(60))
+asyncio.run(timeout_call_concurrent(60))
+
+
