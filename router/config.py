96fd09c06b6de5c1e5441571a581db263c8f523b
manish <mk94mishra@gmail.com>
2023-10-26 10:32:03 +0530

    some changhes

--- router/config.py
+++ router/config.py
@@ -14,13 +14,13 @@ custom = {
 
 proxy_call_limit = 20000
 
-proxy_url ={
-    'proxy_one':'http://127.0.0.1:8000',
-    'proxy_two':'http://18.61.28.63:8000',
-    'proxy_three':'http://127.0.0.1:8000',
-    'proxy_four':'http://18.61.97.72:8000',
-    'proxy_five':'http://18.60.85.51:8000'
-}
+# proxy_url ={
+#     'proxy_one':'http://127.0.0.1:8000',
+#     'proxy_two':'http://18.61.28.63:8000',
+#     'proxy_three':'http://127.0.0.1:8000',
+#     'proxy_four':'http://18.61.97.72:8000',
+#     'proxy_five':'http://18.60.85.51:8000'
+# }
 
 # public ip's proxy
 # proxy_url ={
@@ -32,13 +32,13 @@ proxy_url ={
 # }
 
 # private ip's proxy
-# proxy_url ={
-#     'proxy_one':'http://172.31.17.140:8000',
-#     'proxy_two':'http://172.31.29.170:8000',
-#     'proxy_three':'http://172.31.25.202:8000',
-#     'proxy_four':'http://172.31.16.44:8000',
-#     'proxy_five':'http://172.31.26.33:8000'
-# }
+proxy_url ={
+    'proxy_one':'http://172.31.17.140:8000',
+    'proxy_two':'http://172.31.29.170:8000',
+    'proxy_three':'http://172.31.25.202:8000',
+    'proxy_four':'http://172.31.16.44:8000',
+    'proxy_five':'http://172.31.26.33:8000'
+}
 
 # public ip
 # 18.60.247.215
