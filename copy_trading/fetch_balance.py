e561098481fb52e837f44e179656bacb67dbac29
manish <mk94mishra@gmail.com>
2023-11-14 11:59:32 +0530

    p1

--- copy_trading/fetch_balance.py
+++ copy_trading/fetch_balance.py
@@ -6,39 +6,26 @@ from urllib.parse import urlencode
 
 
 # Replace with your Binance API key and secret
-api_key = 'your_binance_api_key'
-api_secret = 'your_binance_api_secret'
+api_key = 'Nw3cglPnQmqtJVCyjOp84CYfB9T0lcwT9Jm93hPbyAhEgR23mNz8gnkZuPXtfJwJ'
+api_secret = 'bsl0oeBw2sxni3PnOt9PpEnPmKQ9gB3IQVPYlzITTCzeJXGrW2vWAhByQjWFtcbu'
 
 # Replace with your actual proxy URL
 proxy_url = 'http://127.0.0.1:8001/router'
 
-
-# Define order parameters
-symbol = 'BTCUSDT'
-side = 'SELL'
-type_ = 'LIMIT'
-time_in_force = 'GTC'
-quantity = 0.002
-price = 9500
-
+# Binance API endpoint for getting account information
+api_url = 'https://api.binance.com/api/v3/account'
+recv_window = 60000  
 # Construct the query parameters
 params = {
-    'symbol': symbol,
-    'side': side,
-    'type': type_,
-    'timeInForce': time_in_force,
-    'quantity': quantity,
-    'price': price,
     'timestamp': int(time.time() * 1000),
+    'recvWindow': recv_window,
 }
 
 # Create the query string
 query_string = '&'.join([f"{key}={params[key]}" for key in params])
 
+
 def arrange_order(api_key,api_secret,params):
-    # Replace with your Binance API key and secret
-    api_key = 'your_binance_api_key'
-    api_secret = 'your_binance_api_secret'
 
     # Create the signature
     signature = hmac.new(api_secret.encode(), query_string.encode(), hashlib.sha256).hexdigest()
@@ -46,14 +33,14 @@ def arrange_order(api_key,api_secret,params):
     # Include the signature in the query parameters
     params['signature'] = signature
     request_data = {
-            "url":f"/api/v3/order?{urlencode(params)}",
+            "url":f"/api/v3/account?{urlencode(params)}",
             "headers":{"X-MBX-APIKEY": api_key},
-            "method":"POST",
+            "method":"GET",
             "data":None
             }
     # print(request_data)
     return request_data
-    
+
 total_users = 3
 data = []
 for _ in range(total_users):
@@ -61,3 +48,4 @@ for _ in range(total_users):
 print(data)
 response = requests.post(proxy_url, json={'data':data})
 print(response)
+    
